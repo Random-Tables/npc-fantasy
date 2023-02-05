078ae0557f6a1e0cd0304cd89db9bf83c35434fb
aron edwards <aronedwards91@gmail.com>
2023-02-05 09:37:16 +0000

    export calls

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const utilityNpc = require("@random-tables/utility-npc");
 const utilityNpcFantasy = require("@random-tables/utility-npc-fantasy");
 const utilitySenses = require("@random-tables/utility-senses");
 
+const camelize = (s) => s.replace(/-./g, (x) => x[1].toUpperCase());
+const capitalize = (s) => s && s[0].toUpperCase() + s.slice(1);
+
+const calls = {};
+Object.keys(index.tables).forEach((tableGroup) => {
+  index.tables[tableGroup].forEach((table) => {
+    const objectKey = camelize(tableGroup) + capitalize(camelize(table));
+    calls[objectKey] = `${index.collectionID}/${tableGroup}/${table}`;
+  });
+});
+
 module.exports = {
   ...index,
   tableData: {
@@ -19,4 +30,5 @@ module.exports = {
     utilityNpcFantasy,
     utilitySenses,
   ],
+  calls,
 };
